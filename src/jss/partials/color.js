44ffd89e6ec31a9de180f4cb10321d86d5f545ef
Utto <utto660@gmail.com>
2021-08-07 12:58:10 +0300

    Setting up jss and basic theme

---
+++ src/jss/partials/color.js
@@ -0,0 +1,9 @@
+export default {
+  darkGrey: '#263238',
+  dark: '#000a12',
+  lightGrey: '#4f5b62',
+
+  udjatGreen: '#26a69a',
+
+  white: '#fff',
+};
---
+++ src/jss/partials/spacing.js
@@ -0,0 +1,7 @@
+export default {
+  spaceXS: '6px',
+  spaceS: '8px',
+  spaceM: '16px',
+  spaceL: '24px',
+  spaceXL: '32px',
+};
